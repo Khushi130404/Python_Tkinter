97bb080589cfeb344eb58cdd843ef73583d24256
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2024-12-15 14:51:29 +0530

    Rotate Logic

--- Flipkart_Login/wallpaper.py
+++ Flipkart_Login/wallpaper.py
@@ -1,7 +1,15 @@
 from importlib.metadata import files
+from itertools import count
 from tkinter import *
 from PIL import ImageTk,Image
 import os
+
+counter = 1
+
+def rotate():
+    global counter
+    img_label.config(image=img_arr[counter%len(img_arr)])
+    counter += 1
 
 root = Tk()
 root.title("Wallpapers")
@@ -19,7 +27,7 @@ for i in files:
 img_label = Label(root,image=img_arr[0])
 img_label.pack(pady=(30,30))
 
-next_button = Button(root,text="Next",bg='#f4f4f4',fg='black', width=25, height=1)
+next_button = Button(root,text="Next",bg='#f4f4f4',fg='black', width=25, height=1, command=rotate)
 next_button.pack()
 next_button.config(font=('Verdana',12))
 
