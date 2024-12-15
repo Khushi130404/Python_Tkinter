93a581e7cfcd17d0d59ed3c2fce224f917e20ad0
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2024-12-15 14:25:22 +0530

    Image Added

--- Flipkart_Login/login.py
+++ Flipkart_Login/login.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import messagebox
+from PIL import Image,ImageTk
 
 
 def handle_login():
@@ -13,23 +14,29 @@ def handle_login():
 
 root = Tk()
 root.title("Flipkart")
-root.iconbitmap('logo.png')
-root.geometry('350x460')
+root.iconbitmap('logo.svg')
+root.geometry('350x500')
 root.config(background='#0096DC')
 
 text_label = Label(root,text='Flipkart Login',fg='white',bg='#0096DC')
-text_label.pack(pady=(50,10))
+text_label.pack(pady=(30,10))
 text_label.config(font=('Verdana',22))
 
+img = Image.open('flipkart.png')
+img = img.resize((90,90))
+img = ImageTk.PhotoImage(img)
+img_label = Label(root,image=img)
+img_label.pack(pady=(10,10))
+
 email_label = Label(root,text="Enter Email",fg='#f4f4f4',bg='#0096DC')
-email_label.pack(pady=(25,15))
+email_label.pack(pady=(15,10))
 email_label.config(font=('Verdana',12))
 
 email_input = Entry(root,width=40)
 email_input.pack(ipady=6,pady=(1,5))
 
 pass_label = Label(root,text="Enter Password",fg='#f4f4f4',bg='#0096DC')
-pass_label.pack(pady=(25,15))
+pass_label.pack(pady=(15,10))
 pass_label.config(font=('Verdana',12))
 
 pass_input = Entry(root,width=40)
