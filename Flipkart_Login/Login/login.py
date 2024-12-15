a8f8205b83f1f8198e54de0defd653e09ce53cfd
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2024-12-15 13:11:52 +0530

    Password label & Input

--- Flipkart_Login/Login/login.py
+++ Flipkart_Login/Login/login.py
@@ -14,7 +14,15 @@ email_label = Label(root,text="Enter Email",fg='#f4f4f4',bg='#0096DC')
 email_label.pack(pady=(25,15))
 email_label.config(font=('Verdana',12))
 
-email_input = Entry(root,width=50)
+email_input = Entry(root,width=40)
 email_input.pack(ipady=6,pady=(1,5))
+
+pass_label = Label(root,text="Enter Password",fg='#f4f4f4',bg='#0096DC')
+pass_label.pack(pady=(25,15))
+pass_label.config(font=('Verdana',12))
+
+pass_input = Entry(root,width=40)
+pass_input.pack(ipady=6,pady=(1,5))
+
 
 root.mainloop()
